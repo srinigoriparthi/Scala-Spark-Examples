cefca3e021acf8922f007d5e216f75c0c49948b5
srinigoriparthi <srini.goriparthi@gmail.com>
2018-12-06 00:07:36 -0800

    Word Count program formatting.

--- ScalaSparkExamples/src/main/scala/com/gsrao/spark/examples/WordCountExample.scala
+++ ScalaSparkExamples/src/main/scala/com/gsrao/spark/examples/WordCountExample.scala
@@ -9,20 +9,13 @@ object WordCountExample {
   def main(args: Array[String]) = {
 
     //Start the Spark context
-    val conf = new SparkConf()
-      .setAppName("WordCount")
-      .setMaster("local")
+    val conf = new SparkConf().setAppName("WordCount").setMaster("local")
     val sc = new SparkContext(conf) 
 
     //Read some example file to a test RDD
     val test = sc.textFile("D://DEVWORK//WORKSPACES//SampleInputFile.txt")
 
-    test.flatMap { line => //for each line
-      line.split(" ") //split the line in word by word.
-    }
-      .map { word => //for each word
-        (word, 1) //Return a key/value tuple, with the word as key and 1 as value
-      }
+    test.flatMap(line => line.split(" ")).map ( word => (word, 1))
       .reduceByKey(_ + _) //Sum all of the value with same key
       .saveAsTextFile("D://DEVWORK//WORKSPACES//OutPutFile.txt") //Save to a text file
 
