7a63838607fd4c0462506dab34baf7214e6cab29
srinigoriparthi <srini.goriparthi@gmail.com>
2018-12-08 17:33:58 -0800

    Nested Functions

---
+++ ScalaSparkExamples/src/main/scala/com/gsrao/scala/examples/14FunctionsNested.scala
@@ -0,0 +1,26 @@
+package com.gsrao.scala.examples
+
+/**
+ * @author gsrao
+ */
+
+/**
+ * 	Scala is a first class function language which means it allows you to passing function, returning function, composing function, nested function etc. 
+		An example below explain about how to define and call nested functions.
+ */
+object FunctionsNested {
+  
+  def main(args: Array[String]){    
+    var result = addingFunction(20,10,10)
+    println("Result is: "+result)
+  }
+  
+  def addingFunction(a:Int, b:Int, c:Int):Int = {
+    a+anotherNestedFunction(b,c)
+  }
+  
+  def anotherNestedFunction(b:Int, c:Int):Int = {
+    b+c
+  }
+  
+}
