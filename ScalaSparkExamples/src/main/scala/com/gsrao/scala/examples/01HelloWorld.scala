99136c2d7675a9c44aaf2f1110a053772b2fcae1
srinigoriparthi <srini.goriparthi@gmail.com>
2018-12-08 17:57:59 -0800

    Hello World Example with Object

--- ScalaSparkExamples/src/main/scala/com/gsrao/scala/examples/01HelloWorld.scala
+++ ScalaSparkExamples/src/main/scala/com/gsrao/scala/examples/01HelloWorld.scala
@@ -3,6 +3,17 @@ package com.gsrao.scala.examples
 /**
  * @author gsrao
  */
+
+/**
+ * What is an Object?
+    Object is a real world entity. 
+    It contains state and behavior. Laptop, car, cell phone are the real world objects. Object typically has two characteristics:
+    
+    1) State: data values of an object are known as its state.
+    2) Behavior: functionality that an object performs is known as its behavior.
+    3) Object in scala is an instance of class. It is also known as runtime entity.
+ */
+
 object HelloWorld {
   
   def main(args: Array[String]){
