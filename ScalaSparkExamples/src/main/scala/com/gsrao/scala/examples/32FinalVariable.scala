0a6654e453f6f7aafd97e281d470fcc240eb7564
srinigoriparthi <srini.goriparthi@gmail.com>
2018-12-13 22:17:36 -0800

    Final Variables

---
+++ ScalaSparkExamples/src/main/scala/com/gsrao/scala/examples/32FinalVariable.scala
@@ -0,0 +1,52 @@
+package com.gsrao.scala.examples
+
+/*class Hello{
+  final val a:Int = 0
+}
+
+class World extends Hello{
+  // Can not override final variable
+  override val a:Int = 100
+  def show(){
+    println("Value World is:"+a)
+  }
+}
+
+class HelloMethod{
+  final def show(){
+    println("Show HelloMethod Method !!")
+  }
+}
+
+class MyHello extends HelloMethod{
+  // Can not override final Method
+  override def show(){
+    println("Show MyHello Method !!")
+  }
+}
+
+final class MyExample{
+  def show(){
+    println("Show")
+  }
+}
+
+//Illegal Inheritance of Final Class
+class Example1 extends MyExample{
+  def show(){
+    println("Show Example1")
+  }
+}*/
+
+object FinalVariable {
+  def main(args:Array[String]){/*
+    var w = new World()
+    w.show() 
+    
+    var m = new MyHello()
+    m.show()
+    
+    var e = new Example1()
+    e.show()       
+  */}
+}
