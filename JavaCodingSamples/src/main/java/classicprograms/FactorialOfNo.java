246e9d6066517b22163bf6c055fa8c2d1f9414dd
srinigoriparthi <srini.goriparthi@gmail.com>
2019-03-17 18:05:17 -0700

    Formatting.

--- JavaCodingSamples/src/main/java/classicprograms/FactorialOfNo.java
+++ JavaCodingSamples/src/main/java/classicprograms/FactorialOfNo.java
@@ -6,6 +6,7 @@ public class FactorialOfNo {
 
 	public static void main(String[] args) {
 		System.out.println("Enter No:");
+		
 		int num= new Scanner(System.in).nextInt();
 		int result = 1;
 		while(num!=0){
